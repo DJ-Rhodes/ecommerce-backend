0af489b0c849138d3e18d321736302418d24aaaf
DJ-Rhodes <damianjason.rhodes@gmail.com>
2023-03-23 10:56:25 +0200

    Update JWT generation and test
    Updated the authentication of authentication to ensure signing with correct key and issuer. Unit tests added for each as well.

--- src/test/java/com/djrhodes/ecommercebackend/service/JWTServiceTest.java
+++ src/test/java/com/djrhodes/ecommercebackend/service/JWTServiceTest.java
@@ -1,10 +1,15 @@
 package com.djrhodes.ecommercebackend.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.MissingClaimException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.djrhodes.ecommercebackend.model.LocalUser;
 import com.djrhodes.ecommercebackend.model.repository.LocalUserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -21,6 +26,8 @@ public class JWTServiceTest {
     /** The Local User Repository. */
     @Autowired
     LocalUserRepository localUserRepository;
+    @Value("${jwt.algorithm.key}")
+    private String algorithmKey;
 
     /**
      * Tests that the verification token is not usable for login.
@@ -42,4 +49,25 @@ public class JWTServiceTest {
         Assertions.assertEquals(user.getUsername(), jwtService.getUsername(token), "Token for Auth should contain username.");
     }
 
+    /**
+     * Tests that the authentication token was generated with the provided algorithm.
+     */
+    @Test
+    public void testJWTNotGeneratedByUs() {
+        String token = JWT.create().withClaim("USERNAME", "UserA")
+                .sign(Algorithm.HMAC256("Not the correct secret."));
+        Assertions.assertThrows(SignatureVerificationException.class,
+                () -> jwtService.getUsername(token));
+    }
+
+    /**
+     * Tests the that authentication token was issued by the correct issuer.
+     */
+    @Test
+    public void testJWTCorrectlySignedNoIssuer() {
+        String token = JWT.create().withClaim("USERNAME", "UserA")
+                .sign((Algorithm.HMAC256(algorithmKey)));
+        Assertions.assertThrows(MissingClaimException.class,
+                () -> jwtService.getUsername(token));
+    }
 }
